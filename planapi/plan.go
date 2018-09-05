34f87d73107026db8cac75829d2c9d81e65a68ed
YanRyan <527421522@qq.com>
2018-09-05 15:13:25 +0800

    no message

--- planapi/plan.go
+++ planapi/plan.go
@@ -1,6 +1,9 @@
 package planapi
 
 import (
+	"io/ioutil"
+	"net/http"
+	"context"
 	"github.com/gin-gonic/gin"
 	"github.com/asiainfoLDP/servicebroker-plan-api/tools"
 )
@@ -23,8 +26,18 @@ func PollingPlan(c *gin.Context) {
 
 func Deprovision(c *gin.Context) {
 	ins := c.Param("serviceinstance")
+	etcdC := etcdclient.GetEtcdApi()
+	etcdC.Delete(context.Background(),"",)
 }
 
 func Update(c *gin.Context) {
 	ins := c.Param("serviceinstance")
+	rBody, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"status": http.StatusBadRequest, "metrics": err})
+		return
+	}
+	defer c.Request.Body.Close()
+	etcdC := etcdclient.GetEtcdApi()
+	etcdC.Update(context.Background(),)
 }
