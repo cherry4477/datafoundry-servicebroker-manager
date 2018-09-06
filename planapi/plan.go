7ca8fdc96fcd26a39f22f16f023755ac7ed96aeb
YanRyan <527421522@qq.com>
2018-09-06 15:06:09 +0800

    modification map range

--- planapi/plan.go
+++ planapi/plan.go
@@ -391,9 +391,9 @@ func UpdataService(c *gin.Context) {
 	req := &client.Response{}
 	for _,v := range pservice.Services{
 		mValue := getTag(&v)
-		for k,v := range mValue{
-			key += "/" + k
-			req,err = etcdC.Update(context.Background(),key,v)
+		for mk,mv := range mValue{
+			key += "/" + mk
+			req,err = etcdC.Update(context.Background(),key,mv)
 			if err != nil{
 				log.Logger.Error("Can not UpdataService service from etcd", err)
 				errinfo := ErrorResponse{}
@@ -428,9 +428,9 @@ func UpdataPlan(c *gin.Context) {
 	req := &client.Response{}
 	for _,v := range pservice.Plans{
 		mValue := getTag(&v)
-		for k,v := range mValue{
-			key += "/" + k
-			req,err = etcdC.Update(context.Background(),key,v)
+		for mk,mv := range mValue{
+			key += "/" + mk
+			req,err = etcdC.Update(context.Background(),key,mv)
 			if err != nil{
 				log.Logger.Error("Can not UpdataPlan service from etcd", err)
 				errinfo := ErrorResponse{}
